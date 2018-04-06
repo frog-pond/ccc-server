4a564bc51cc9c30624983070c87641f39c6f335c
Drew Volz <volz.drew@gmail.com>
2018-04-06 06:49:18 -0600

    rename get calls to represent cached length

--- v1/menu/index.mjs
+++ v1/menu/index.mjs
@@ -3,14 +3,14 @@ import mem from 'mem'
 
 const ONE_HOUR = 60 * 60 * 1000
 const ONE_DAY = 24 * 60 * 60 * 1000
-const GET_MENU = mem(got.get, {maxAge: ONE_HOUR})
-const GET_CAFE = mem(got.get, {maxAge: ONE_DAY})
+const GET_HOUR = mem(got.get, {maxAge: ONE_HOUR})
+const GET_DAY = mem(got.get, {maxAge: ONE_DAY})
 
 const menuBase = 'http://legacy.cafebonappetit.com/api/2/menus'
 const cafeBase = 'http://legacy.cafebonappetit.com/api/2/cafes'
 
-export const getCafeMenu = (cafeId) => GET_MENU(menuBase, {json: true, query: {cafe: cafeId}})
-export const getCafeInfo = (cafeId) => GET_CAFE(cafeBase, {json: true, query: {cafe: cafeId}})
+export const getCafeMenu = (cafeId) => GET_HOUR(menuBase, {json: true, query: {cafe: cafeId}})
+export const getCafeInfo = (cafeId) => GET_DAY(cafeBase, {json: true, query: {cafe: cafeId}})
 export const getCafe = (cafeId) => Promise.all([getCafeMenu(cafeId), getCafeInfo(cafeId)])
 
 export async function menu(ctx) {
