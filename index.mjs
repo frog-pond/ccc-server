9459bf0b1ee03c161d149458e1e166d7451269be
Hawken Rives <hawkrives@gmail.com>
2018-04-29 21:16:38 -0500

    remove koa-ping

--- index.mjs
+++ index.mjs
@@ -3,7 +3,6 @@ import etag from 'koa-etag'
 import compress from 'koa-compress'
 import logger from 'koa-logger'
 import responseTime from 'koa-response-time'
-import health from 'koa-ping'
 import Router from 'koa-router'
 import Koa from 'koa'
 
@@ -23,6 +22,10 @@ async function main() {
 		ctx.body = `Hello world! Prefix: ${ctx.route.prefix}`
 	})
 
+	router.get('/ping', async ctx => {
+		ctx.body = 'pong'
+	})
+
 	// router.getRoutes().forEach(route => console.log(route.path))
 
 	//
@@ -34,7 +37,6 @@ async function main() {
 	// etag works together with conditional-get
 	app.use(conditional())
 	app.use(etag())
-	app.use(health())
 	// hook in the router
 	app.use(router.routes())
 	app.use(router.allowedMethods())
