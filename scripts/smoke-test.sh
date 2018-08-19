1bc8a9e81c1f26409d8ef149fa887afbfa92c6e5
Hawken Rives <hawkrives@fastmail.fm>
2018-08-18 20:29:09 -0500

    add a script to test the server and make sure it launches and responds to /ping

---
+++ scripts/smoke-test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# exit the script if any command exits
+set -e
+
+# kill the servers if the script exits
+trap "exit" INT TERM
+trap "kill 0" EXIT
+
+# check that the server can launch properly, but don't bind to a port
+env SMOKE_TEST=1 npm run stolaf
+
+# launch and background the server, so we can test it
+PORT=3000
+env NODE_PORT=$PORT npm run stolaf &
+
+# wait while the server starts up
+while [[ ! $(lsof -i :$PORT) ]]; do
+	sleep 0.1
+done
+
+# run the test
+TEST=$(curl -s localhost:3000/ping)
+
+# assert that the /ping endpoint responded with "pong"
+if [[ ! $TEST -eq pong ]]; then
+	exit 1
+fi
