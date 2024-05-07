0ab2c9d7fa1df4aa7258afeafd240900eed74c82
Hawken Rives <hawkrives@fastmail.fm>
2024-05-07 16:54:17 -0400

    p-memoize stolaf/transit

--- source/ccci-stolaf-college/v1/transit.js
+++ source/ccci-stolaf-college/v1/transit.js
@@ -1,13 +1,13 @@
 import {get} from '../../ccc-lib/http.js'
 import {ONE_HOUR} from '../../ccc-lib/constants.js'
-import mem from 'memoize'
 import {GH_PAGES} from './gh-pages.js'
+import pMemoize from 'p-memoize'
+import {ONE_HOUR_CACHE} from '../../ccc-lib/cache.js'
 
-const GET = mem(get, {maxAge: ONE_HOUR})
-
-export function getBus() {
-	return GET(GH_PAGES('bus-times.json')).json()
-}
+const _getBus = () => get(GH_PAGES('bus-times.json')).json()
+export const getBus = pMemoize(_getBus, {
+	cache: ONE_HOUR_CACHE,
+})
 
 export async function bus(ctx) {
 	ctx.cacheControl(ONE_HOUR)
@@ -15,9 +15,10 @@ export async function bus(ctx) {
 	ctx.body = await getBus()
 }
 
-export function getModes() {
-	return GET(GH_PAGES('transportation.json')).json()
-}
+const _getModes = () => get(GH_PAGES('transportation.json')).json()
+export const getModes = pMemoize(_getModes, {
+	cache: ONE_HOUR_CACHE,
+})
 
 export async function modes(ctx) {
 	ctx.cacheControl(ONE_HOUR)
