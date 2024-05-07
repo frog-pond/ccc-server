5933d66836c9b2b671c093eacf014397dd5e3098
Hawken Rives <hawkrives@fastmail.fm>
2024-05-07 16:52:49 -0400

    p-memoize carleton/contacts

--- source/ccci-carleton-college/v1/contacts.js
+++ source/ccci-carleton-college/v1/contacts.js
@@ -1,15 +1,11 @@
 import {get} from '../../ccc-lib/http.js'
 import {ONE_HOUR} from '../../ccc-lib/constants.js'
-import mem from 'memoize'
 import {GH_PAGES} from './gh-pages.js'
+import pMemoize from 'p-memoize'
+import {ONE_HOUR_CACHE} from '../../ccc-lib/cache.js'
 
-const GET = mem(get, {maxAge: ONE_HOUR})
-
-let url = GH_PAGES('contact-info.json')
-
-export function getContacts() {
-	return GET(url).json()
-}
+const _getContacts = () => get(GH_PAGES('contact-info.json')).json()
+export const getContacts = pMemoize(_getContacts, {cache: ONE_HOUR_CACHE})
 
 export async function contacts(ctx) {
 	ctx.cacheControl(ONE_HOUR)
