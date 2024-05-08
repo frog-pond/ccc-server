f3eebc0241449ed8d64d8a8a9ed826748e21c10e
Hawken Rives <hawkrives@fastmail.fm>
2024-05-07 21:07:12 -0400

    migrate "presence" module to typescript

---
+++ source/student-orgs/types.ts
@@ -0,0 +1,33 @@
+import {z} from 'zod'
+
+export type ContactPersonType = z.infer<typeof ContactPersonSchema>
+export const ContactPersonSchema = z.object({
+	lastName: z.string(),
+	firstName: z.string(),
+	title: z.string(),
+	email: z.string().email(),
+})
+
+export type AdvisorType = z.infer<typeof AdvisorSchema>
+export const AdvisorSchema = z.object({
+	name: z.string(),
+	email: z.string().email(),
+})
+
+export type StudentOrgType = z.infer<typeof StudentOrgSchema>
+export const StudentOrgSchema = z.object({
+	meetings: z.string(),
+	contacts: ContactPersonSchema.array(),
+	advisors: AdvisorSchema.array(),
+	description: z.string(),
+	category: z.string(),
+	lastUpdated: z.string().datetime(),
+	website: z.string().url(),
+	name: z.string().min(1),
+})
+
+export type SortableStudentOrgType = z.infer<typeof SortableStudentOrgSchema>
+export const SortableStudentOrgSchema = StudentOrgSchema.extend({
+	$sortableName: z.string(),
+	$groupableName: z.string(),
+})
