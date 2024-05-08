3ba13787db59fbe944c16f68fce566fe7a17999d
Hawken Rives <hawkrives@fastmail.fm>
2024-05-08 09:06:15 -0400

    migrating ccc-calendar to ts

---
+++ source/calendar/ical.ts
@@ -0,0 +1,57 @@
+import {get} from '../ccc-lib/http.js'
+import moment from 'moment'
+import getUrls from 'get-urls'
+import {JSDOM} from 'jsdom'
+import InternetCalendar from 'ical.js'
+import {EventSchema} from './types.js'
+import {sortBy} from 'lodash'
+
+function convertEvent(event: InternetCalendar.Event, now = moment()) {
+	const startTime = moment(event.startDate.toString())
+	const endTime = moment(event.endDate.toString())
+	let description =
+		JSDOM.fragment(event.description || '').textContent?.trim() ?? ''
+
+	return EventSchema.parse({
+		dataSource: 'ical',
+		startTime: startTime.toISOString(),
+		endTime: endTime.toISOString(),
+		title: event.summary,
+		description: description,
+		location: event.location,
+		isOngoing: startTime.isBefore(now, 'day'),
+		links: [...getUrls(description)],
+		metadata: {
+			uid: event.uid,
+		},
+		config: {
+			startTime: true,
+			endTime: true,
+			subtitle: 'location',
+		},
+	})
+}
+
+export async function ical(
+	url: string | URL,
+	{onlyFuture = true} = {},
+	now = moment(),
+) {
+	let body = await get(url, {headers: {accept: 'text/calendar'}}).text()
+
+	let comp = InternetCalendar.Component.fromString(body)
+	let events = comp
+		.getAllSubcomponents('vevent')
+		.map((vevent) => new InternetCalendar.Event(vevent))
+
+	if (onlyFuture) {
+		events = events.filter((event) =>
+			moment(event.endDate.toString()).isAfter(now, 'day'),
+		)
+	}
+
+	return sortBy(
+		events.map((e) => convertEvent(e, now)),
+		(e) => e.startTime,
+	)
+}
--- source/calendar/types.ts
+++ source/calendar/types.ts
@@ -1,12 +1,12 @@
 import {z} from 'zod'
 
-const EventConfig = z.object({
+const EventConfigSchema = z.object({
 	startTime: z.boolean(),
 	endTime: z.boolean(),
 	subtitle: z.union([z.literal('location'), z.literal('description')]),
 })
 
-export const Event = z.object({
+export const EventSchema = z.object({
 	dataSource: z.string(),
 	startTime: z.string().datetime(),
 	endTime: z.string().datetime(),
@@ -15,6 +15,6 @@ export const Event = z.object({
 	location: z.string().default(''),
 	isOngoing: z.boolean(),
 	links: z.array(z.unknown()),
-	config: EventConfig,
+	config: EventConfigSchema,
 	metadata: z.optional(z.unknown()),
 })
