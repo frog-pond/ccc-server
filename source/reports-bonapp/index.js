d5905bdb88d90825a9e1bf574262e2878444677a
Drew Volz <volz.drew@gmail.com>
2024-05-18 19:33:01 -0700

    add promise to wait for window fetch to finish
    * wrap and resolve promise and return parsed data
    * add custom report type in error
    * exclude chart error from console

--- source/reports-bonapp/index.js
+++ source/reports-bonapp/index.js
@@ -5,6 +5,7 @@ import {JSDOM, VirtualConsole} from 'jsdom'
 import mem from 'memoize'
 import * as Sentry from '@sentry/node'
 
+import {CustomReportType} from './helpers.js'
 import {StavReportType} from './types.js'
 
 const getBonAppPage = mem(get, {maxAge: ONE_MINUTE})
@@ -24,7 +25,6 @@ async function getBonAppReportWebpage(url) {
 			'Uncaught [ReferenceError: jQuery is not defined]',
 			'Uncaught [Error: Create skia surface failed]',
 			"Uncaught [TypeError: Cannot read properties of undefined (reading 'slice')]",
-			"Failed to create chart: can't acquire context from the given item",
 			'Not implemented: HTMLCanvasElement.prototype.getContext (without installing the canvas npm package)',
 		]
 		if (jsdomErrorMessagesToSkip.includes(err.message)) {
@@ -51,6 +51,16 @@ async function getBonAppReportWebpage(url) {
 async function _report(reportUrl) {
 	let dom = await getBonAppReportWebpage(reportUrl)
 
+	dom.window.console.error = ((error) => { 
+		let errorMessagesToSkip = [
+			"Failed to create chart: can't acquire context from the given item",
+		]
+		if (errorMessagesToSkip.includes(error)) {
+			return
+		}
+		console.error(error)
+	})
+
 	dom.window.console.info = ((info) => { 
 		let infoMessagesToSkip = [
 			'Initialized global navigation scripts',
@@ -64,34 +74,32 @@ async function _report(reportUrl) {
 		console.info(info)
 	})
 
-	dom.window.onload = () => {
-		const charts = dom.window.Chart.instances
+	return new Promise((resolve, reject) => {
+		dom.window.onload = () => {
+			const charts = dom.window.Chart.instances
 
-		const parse = (chart) => {
-			const {labels, datasets} = chart.data
-			return {
-				title: datasets[0].label,
-				times: labels,
-				data: datasets[0].data,
+			const parse = (chart) => {
+				const {labels, datasets} = chart.data
+				return {
+					title: datasets[0].label,
+					times: labels,
+					data: datasets[0].data,
+				}
 			}
-		}
 
-		const payload = []
+			const payload = []
 
-		for(let i=0; i<NUMBER_OF_CHARTS_TO_PARSE; ++i) {
-			try {
-				payload.push(parse(charts[i]))
-			} catch (err) {
-				console.warn({error: err.message})
+			for(let i=0; i<NUMBER_OF_CHARTS_TO_PARSE; ++i) {
+				try {
+					payload.push(parse(charts[i]))
+				} catch (err) {
+					console.warn({error: err.message})
+				}
 			}
+
+			resolve(StavReportType.parse(payload));
 		}
-
-		// TODO: we have data here but we can't return it back fast enough
-		// while in the dom.window.onload callback to the `report` function
-		console.log(StavReportType.parse(payload))
-
-		return StavReportType.parse(payload)
-	}
+	})
 }
 
 /**
@@ -104,6 +112,6 @@ export function report(reportUrl) {
 	} catch (err) {
 		console.error(err)
 		Sentry.isInitialized() && Sentry.captureException(err)
-		return CustomCafe({message: 'Could not load BonApp report'})
+		return CustomReportType({message: 'Could not load BonApp report'})
 	}
 }
