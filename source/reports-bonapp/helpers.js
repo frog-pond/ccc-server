330a861a539c847ddf55da981c0a560e3d18a2ab
Drew Volz <volz.drew@gmail.com>
2024-05-18 19:33:56 -0700

    add custom report helper

---
+++ source/reports-bonapp/helpers.js
@@ -0,0 +1,10 @@
+import {StavReportType} from './types.js'
+
+/** @returns {StavReportType} */
+export function CustomReportType({message}) {
+	return StavReportType.parse([{
+                title: message,
+                times: [],
+                data: [],
+        }])
+}
