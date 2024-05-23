76f4017085e793fd48264cb57badd724232c08d5
Drew Volz <volz.drew@gmail.com>
2024-05-23 04:20:10 +0000

    switch stav mealtime report to gh pages

--- source/ccci-stolaf-college/v1/gh-pages.ts
+++ source/ccci-stolaf-college/v1/gh-pages.ts
@@ -1,2 +1,13 @@
-export const GH_PAGES = (filename: string) =>
-	new URL(`https://stodevx.github.io/AAO-React-Native/${filename}`)
+const BASE_URL = `https://stodevx.github.io`
+
+export const GH_PAGES = (filename: string) => {
+	const url = new URL(BASE_URL)
+	url.pathname = `/AAO-React-Native/${filename}`
+	return url
+}
+
+export const GH_PAGES_FROM_REPO = (reponame: string, filename: string, ) => {
+	const url = new URL(BASE_URL)
+	url.pathname = `${reponame}/${filename}`
+	return url
+}
--- source/ccci-stolaf-college/v1/reports.ts
+++ source/ccci-stolaf-college/v1/reports.ts
@@ -1,16 +1,19 @@
+import {get} from '../../ccc-lib/http.js'
 import {ONE_HOUR} from '../../ccc-lib/constants.js'
-import * as report from '../../reports-bonapp/index.js'
+import {GH_PAGES_FROM_REPO} from './gh-pages.js'
 import mem from 'memoize'
 import type {Context} from '../../ccc-server/context.js'
 
-const getReport = mem(report.report, {maxAge: ONE_HOUR})
+const GET = mem(get, {maxAge: ONE_HOUR})
 
-export const REPORT_URLS = {
-	stav: 'https://www.stolaf.edu/apps/mealtimes/',
-} as const
+let stavMealtimesUrl = GH_PAGES_FROM_REPO('stav-mealtimes', 'two-weeks.json')
+
+export function getStavMealtimes() {
+	return GET(stavMealtimesUrl).json()
+}
 
 export async function stavMealtimeReport(ctx: Context) {
 	ctx.cacheControl(ONE_HOUR)
 
-	ctx.body = await getReport(REPORT_URLS.stav)
+	ctx.body = await getStavMealtimes()
 }
