767048d14cf4624f8e4a951d15b66093948422bf
Drew Volz <volz.drew@gmail.com>
2024-05-26 01:14:06 -0700

    prettier formatting for scripts

--- scripts/watch.js
+++ scripts/watch.js
@@ -4,9 +4,9 @@ const buildWatch = spawn('npm', ['run', 'build:watch'], {stdio: 'inherit'})
 const startWatch = spawn('npm', ['run', 'start:watch'], {stdio: 'inherit'})
 
 const shutdown = () => {
-  buildWatch.kill()
-  startWatch.kill()
-  process.exit()
+	buildWatch.kill()
+	startWatch.kill()
+	process.exit()
 }
 
 process.on('SIGINT', shutdown)
