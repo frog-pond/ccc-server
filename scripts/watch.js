b1d8e4d04178c3a2c231b5b3d9c2fc42e64f440a
Drew Volz <volz.drew@gmail.com>
2024-05-27 00:09:14 -0700

    script renames and formatting changes (#751)
    * rename start:watch and build:watch scripts
    * rename test:stolaf-college and test:carleton-college scripts
    * add scripts to the prettier config and format watch.js
    * formatting changes to node.js.yml

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,12 +1,12 @@
 import {spawn} from 'child_process'
 
-const buildWatch = spawn('npm', ['run', 'build-watch'], {stdio: 'inherit'})
-const startWatch = spawn('npm', ['run', 'start-watch'], {stdio: 'inherit'})
+const buildWatch = spawn('npm', ['run', 'build:watch'], {stdio: 'inherit'})
+const startWatch = spawn('npm', ['run', 'start:watch'], {stdio: 'inherit'})
 
 const shutdown = () => {
-  buildWatch.kill()
-  startWatch.kill()
-  process.exit()
+	buildWatch.kill()
+	startWatch.kill()
+	process.exit()
 }
 
 process.on('SIGINT', shutdown)
