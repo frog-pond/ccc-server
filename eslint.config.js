29928aa0e90b17108d25b182205b07ded165ef46
Hawken Rives <hawkrives@fastmail.fm>
2025-10-07 22:50:51 +0000

    remove unneeded eslint-config-prettier package
    it disables stylistic eslint rules, but neither eslint nor ts-eslint enable any of those.

--- eslint.config.js
+++ eslint.config.js
@@ -4,13 +4,11 @@ import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import globals from 'globals'
-import prettier from 'eslint-config-prettier'
 
 export default defineConfig(
 	eslint.configs.recommended,
 	tseslint.configs.strictTypeChecked,
 	tseslint.configs.stylisticTypeChecked,
-	prettier,
 	{
 		languageOptions: {
 			ecmaVersion: 'latest',
